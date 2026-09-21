120cc7205ab8039e30f33403de76a1f20fa4decc
jaydeep raijada <j.raijada25@gmail.com>
2026-09-21 18:39:09 +0530

    code positional embed module

--- model.py
+++ model.py
@@ -11,4 +11,28 @@ class InputEmbeddings(nn.Module):
         self.embedding = nn.Embedding(vocab_size, d_model)
 
     def forward(self, x):
-        return self.embedding(x) * math.sqrt(self.d_model) #done in the paper, not sure why
+        return self.embedding(x) * math.sqrt(self.d_model) #done in the paper, not sure why ("In the embedding layers, we multiply those weights by √dmodel.")
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout =nn.Dropout(dropout)
+
+        #create a matrix of shape (seq_len, d_model)
+        pe = torch.zeros(seq_len, d_model)
+        #create a vector of shape (seq_len)
+        position = torch.arange(0, seq_len, dtype = torch.float).unsqueeze(1) # (seq_len, 1) 
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model)) # (d_model/2)
+        #Apply the sin to even positions
+        pe[:, 0::2] = torch.sin(position * div_term) # (seq_len, d_model/2)
+        #Apply the cos to odd positions
+        pe[:, 1::2] = torch.cos(position * div_term) # (seq_len, d_model/2)
+        pe = pe.unsqueeze(0) # (1, seq_len, d_model)
+
+        self.register_buffer('pe', pe) #register the positional encoding as a buffer, so it is not a parameter of the model
+
+    def forward(self, x):
+        x = x + self.pe[:, :x.shape[1], :].requires_grad_(False) #add the positional encoding to the input
+        return self.dropout(x) #apply dropout to the input
